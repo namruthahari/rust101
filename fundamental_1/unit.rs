86a0320363b2c15b208e4c73cb3e6984d7f8defd
namruthari <clickfornamz@gmail.com>
2022-07-20 23:10:47 +0530

    ex1

--- fundamental_1/unit.rs
+++ fundamental_1/unit.rs
@@ -1,10 +1,9 @@
 
-// Make it work, don't modify `implicitly_ret_unit` !
 fn main() {
     let _v: () = ();
 
-    let v = (2, 3);
-    assert_eq!(v, implicitly_ret_unit());
+    // let v = (2, 3);
+    assert_eq!(_v, implicitly_ret_unit());
 
     println!("Success!");
 }
@@ -13,7 +12,7 @@ fn implicitly_ret_unit() {
     println!("I will return a ()");
 }
 
-// Don't use this one
-fn explicitly_ret_unit() -> () {
-    println!("I will return a ()");
-}
+// // Don't use this one
+// fn explicitly_ret_unit() -> () {
+//     println!("I will return a ()");
+// }
